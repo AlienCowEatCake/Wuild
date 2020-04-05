0b8547430c30ba1f1f56145341183d0ab8b3a473
Peter Zhigalov <peter.zhigalov@gmail.com>
2020-04-05 20:59:31 +0700

    Add extended toolserver info to WuildCoordinatorStatus

--- WuildCoordinatorStatus.cpp
+++ WuildCoordinatorStatus.cpp
@@ -14,11 +14,81 @@
 #include "AppUtils.h"
 
 #include <CoordinatorClient.h>
+#include <RemoteToolFrames.h>
+#include <SocketFrameHandler.h>
 #include <StringUtils.h>
 #include <TimePoint.h>
 
 #include <iostream>
 #include <set>
+
+namespace Wuild {
+
+class RemoteToolVersionClient
+{
+public:
+	using Config = RemoteToolClientConfig;
+	using VersionInfoArrivedCallback = std::function<void(const std::map<std::string, std::string>&, const ToolServerInfo&)>;
+
+public:
+	RemoteToolVersionClient() = default;
+	~RemoteToolVersionClient() = default;
+
+	bool SetConfig(const Config & config)
+	{
+		std::ostringstream os;
+		if (!config.Validate(&os))
+		{
+			Syslogger(Syslogger::Err) << os.str();
+			return false;
+		}
+		m_config = config;
+		return true;
+	}
+
+	void AddClient(const ToolServerInfo & info)
+	{
+		const std::lock_guard<std::mutex> lock(m_clientsMutex);
+		SocketFrameHandlerSettings settings;
+		settings.m_channelProtocolVersion = RemoteToolRequest::s_version + RemoteToolResponse::s_version;
+		settings.m_segmentSize = 8192;
+		settings.m_hasConnStatus = true;
+		SocketFrameHandler::Ptr handler(new SocketFrameHandler(settings));
+		handler->RegisterFrameReader(SocketFrameReaderTemplate<ToolsVersionResponse>::Create());
+		handler->SetTcpChannel(info.m_connectionHost, info.m_connectionPort);
+
+		auto versionFrameCallback = [this, info](SocketFrame::Ptr responseFrame, SocketFrameHandler::ReplyState state, const std::string & errorInfo)
+		{
+			if (state == SocketFrameHandler::ReplyState::Timeout || state == SocketFrameHandler::ReplyState::Error)
+			{
+				Syslogger(Syslogger::Err) << "Error on requesting toolserver " << info.m_connectionHost << ", " << errorInfo;
+			}
+			else if (m_versionInfoArrivedCallback)
+			{
+				ToolsVersionResponse::Ptr result = std::dynamic_pointer_cast<ToolsVersionResponse>(responseFrame);
+				m_versionInfoArrivedCallback(result->m_versions, info);
+			}
+		};
+
+		handler->QueueFrame(ToolsVersionRequest::Ptr(new ToolsVersionRequest()), versionFrameCallback, m_config.m_requestTimeout);
+		m_clients.push_back(handler);
+		handler->Start();
+	}
+
+	void SetVersionInfoArrivedCallback(VersionInfoArrivedCallback callback)
+	{
+		m_versionInfoArrivedCallback = std::move(callback);
+	}
+
+private:
+	std::mutex m_clientsMutex;
+	std::deque<SocketFrameHandler::Ptr> m_clients;
+	VersionInfoArrivedCallback m_versionInfoArrivedCallback;
+	Config m_config;
+};
+
+}
+
 
 int main(int argc, char** argv)
 {
@@ -33,8 +103,12 @@ int main(int argc, char** argv)
 	if (!client.SetConfig(config))
 		return 1;
 
+	RemoteToolVersionClient toolVersionClient;
+	const bool toolVersionClientConfigured = toolVersionClient.SetConfig(app.m_remoteToolClientConfig);
+	std::mutex toolVersionClientLogMutex;
+	std::atomic_size_t toolVersionClientWaitCounter{0};
 
-	client.SetInfoArrivedCallback([](const CoordinatorInfo& info){
+	client.SetInfoArrivedCallback([&](const CoordinatorInfo& info){
 		std::cout << "Coordinator connected tool servers: \n";
 
 		std::map<int64_t, int> sessionsUsed;
@@ -72,7 +146,31 @@ int main(int argc, char** argv)
 		for (const auto & t : toolIds)
 			std::cout << t << ", ";
 		std::cout << "\n";
-		Application::Interrupt(0);
+
+		std::flush(std::cout);
+		if (toolVersionClientConfigured)
+		{
+			for (const ToolServerInfo & toolServer : info.m_toolServers)
+			{
+				++toolVersionClientWaitCounter;
+				toolVersionClient.AddClient(toolServer);
+			}
+		}
+
+		if (!toolVersionClientConfigured || info.m_toolServers.empty())
+			Application::Interrupt(0);
+	});
+
+	toolVersionClient.SetVersionInfoArrivedCallback([&](const std::map<std::string, std::string> & versionMap, const ToolServerInfo & info){
+		const std::lock_guard<std::mutex> lock(toolVersionClientLogMutex);
+
+		std::cout << "\n" << info.m_connectionHost << ":" << info.m_connectionPort << ":\n";
+		for (const auto & versionInfo : versionMap)
+			std::cout << "  " << versionInfo.first << ", version = \"" << versionInfo.second << "\"\n";
+		std::flush(std::cout);
+
+		if (--toolVersionClientWaitCounter == 0)
+			Application::Interrupt(0);
 	});
 
 	client.Start();
